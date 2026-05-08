645ad4db75ac1796a59f59e6fd379ccc5f807fc1
Daniel Dao <danieldkdao@gmail.com>
2026-05-08 02:35:42 -0500

    feat: implement arena page and adjust dashboard layout container dimensions

--- src/app/(main)/arena/page.tsx
+++ src/app/(main)/arena/page.tsx
@@ -1,44 +1,35 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { LandmarkIcon, SparklesIcon, UsersIcon } from "lucide-react";
+import { SwordsIcon, ViewIcon } from "lucide-react";
 import Link from "next/link";
 
 const options = [
   {
-    title: "Arena",
-    description:
-      "Ready to face off against other developers to learn and improve your skills? Go to arena to get started!",
-    icon: LandmarkIcon,
-    href: "/arena",
+    title: "Compete",
+    description: "We'll match you in a one-to-one battle to test your skills!",
+    icon: SwordsIcon,
+    href: "/arena/matches/compete",
   },
   {
-    title: "Match Up",
-    description:
-      "Need a teammate for a hackathon or want to find some coding pals? Discover other developers and groups to collaborate and have fun.",
-    icon: UsersIcon,
-    href: "/connect",
-  },
-  {
-    title: "The Oracle",
-    description:
-      "Feeling shaky on some concepts? Head to the oracle to get personalized traning and feedback to improve your skills.",
-    icon: SparklesIcon,
-    href: "/oracle",
+    title: "Observe",
+    description: "Watch others' matches to learn and see the action unfold!",
+    icon: ViewIcon,
+    href: "/arena/matches/watch",
   },
 ];
 
-const DashboardPage = () => {
+const ArenaPage = () => {
   return (
     <div className="w-full h-full pt-10 px-6 flex items-center justify-center overflow-y-auto">
       <div className="w-full max-w-250 flex flex-col items-center gap-4">
         <div className="flex flex-col items-center gap-1">
           <h1 className="text-3xl font-bold text-center">
-            Good to see you again!
+            Welcome to the arena!
           </h1>
           <p className="text-base text-muted-foreground text-center">
-            What are you feeling like today?
+            What do you want to do?
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center w-full">
           {options.map((option) => (
             <Link href={option.href} key={option.href} className="h-full">
               <Card className="h-full">
@@ -63,4 +54,4 @@ const DashboardPage = () => {
   );
 };
 
-export default DashboardPage;
+export default ArenaPage;
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="pb-20 bg-radial from-primary/10 via-primary/20 to-primary/10">
+    <div className="pb-20 bg-radial w-full h-svh from-primary/10 via-primary/20 to-primary/10">
       {children}
       <CommandFooter />
     </div>
